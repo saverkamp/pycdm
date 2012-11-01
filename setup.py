2b67d2778f3a76e4094f55acabd95f921517f835
Shawn Averkamp <shawnaverkamp@gmail.com>
2012-11-01 10:37:48 -0500

    Initial setup.py file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils import setup
+
+setup(
+	name='Pycdm',
+	version='0.1',
+	packages=['pycdm'],
+	license='GNU General Public License, version 3.0',
+	long_description=open('README.md').read(),
+)
