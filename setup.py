80d289a9b84e232f13082464c67b131f05fa3f82
Shawn Averkamp <shawnaverkamp@gmail.com>
2014-03-01 21:53:40 -0600

    Update changelog and setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pycdm',
-    version='0.1.4',
+    version='0.2',
     author='Shawn Averkamp',
     author_email='shawnaverkamp@gmail.com',
     url='http://pypi.python.org/pypi/pycdm',
